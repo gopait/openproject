55c8afd913993e4fcb904b5554221049b39b955f
Jens Ulferts <j.ulferts@finn.de>
2014-06-09 15:06:53 +0200

    indentation

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -155,8 +155,11 @@ describe ApplicationHelper do
     let(:project_member) { FactoryGirl.create :user,
                                               :member_in_project => project,
                                               :member_through_role => FactoryGirl.create(:role,
-                                                                                         :permissions => [:view_work_packages, :edit_work_packages,
-                                                                                         :browse_repository, :view_changesets, :view_wiki_pages]) }
+                                                                                         :permissions => [:view_work_packages,
+                                                                                                          :edit_work_packages,
+                                                                                                          :browse_repository,
+                                                                                                          :view_changesets,
+                                                                                                          :view_wiki_pages]) }
     let(:issue) { FactoryGirl.create :work_package,
                                      :project => project,
                                      :author => project_member,
